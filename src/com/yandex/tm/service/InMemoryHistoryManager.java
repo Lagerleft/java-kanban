a588c6b518c939359695f51deb6d4c8a3860365d
Валинька <Lagerleft@yandex.ru>
2024-10-14 19:16:00 +0300

    project version #14 Sixth version for Sprint 5. Minor fixes

--- src/com/yandex/tm/service/InMemoryHistoryManager.java
+++ src/com/yandex/tm/service/InMemoryHistoryManager.java
@@ -8,19 +8,14 @@ import java.util.List;
 public class InMemoryHistoryManager implements HistoryManager {
 
     private static final int HISTORY_SIZE = 10;
-    private static int entrySlot = 0;
     private final List<Task> history = new ArrayList<>();
 
     @Override
     public void add(Task task) {
-        if (entrySlot == HISTORY_SIZE) {
-            entrySlot = 0;
-        }
         if (history.size() == HISTORY_SIZE) {
-            history.remove(entrySlot);
+            history.removeFirst();
         }
-        history.add(entrySlot, task);
-        entrySlot++;
+        history.add(task);
     }
 
 
